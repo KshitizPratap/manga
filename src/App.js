8080f60d60042e8ede021ce6620125c46e3f2159
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2024-01-27 00:57:03 +0530

    basic str

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import classes from './App.module.css';
+import classes from "./App.module.css";
+import { images_Routes } from "./constant";
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className={classes.mainContainer}>
+      <div className={classes.header}></div>
+      <div className={classes.imgContainer}>
+        {images_Routes.map((vid_src, i) => {
+          return (
+            <div className={classes.videoContainer}>
+              <video src={vid_src} key={i} autoPlay muted loop></video>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,27 @@
+import Img1 from "./assets/1.mp4";
+import Img2 from "./assets/2.mp4";
+import Img3 from "./assets/3.mp4";
+import Img4 from "./assets/4.mp4";
+import Img5 from "./assets/5.mp4";
+import Img6 from "./assets/6.mp4";
+import Img7 from "./assets/7.mp4";
+import Img8 from "./assets/8.mp4";
+import Img9 from "./assets/9.mp4";
+import Img10 from "./assets/10.mp4";
+import Img11 from "./assets/11.mp4";
+import Img12 from "./assets/12.mp4";
+
+export const images_Routes = [
+  Img1,
+  Img2,
+  Img3,
+  Img4,
+  Img5,
+  Img6,
+  Img7,
+  Img8,
+  Img9,
+  Img10,
+  Img11,
+  Img12,
+];
